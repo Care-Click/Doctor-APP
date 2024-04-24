f60fee956477cdf18618725f8d519af9578b79df
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-24 10:09:20 +0100

    setting new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import React from 'react'
 import Navbar from "./components/Navbar";
+import LoggedNavbar from "./components/loggedNavbar.tsx";
 import Login from "./components/auth/Login.tsx";
 import SignUp from "./components/auth/SignUp.tsx";
-
+import Home from "./components/Home.tsx"
+import Requests from "./components/patient/Requests.jsx";
 
 const App = () => {
+  let token=localStorage.getItem('token')
   return (
     <div className="App" >
-      <Navbar/>
+      {!token?<Navbar/>:<LoggedNavbar/>}
       <Routes>
-        <Route path="/login" element={<Login login={Login} />} />
+        <Route path="/login" element={<Login />} />
         {/* <Route path="/contact" element={<Contact />} /> */}
         <Route path="/join-us" element={<SignUp />} />
+        <Route path="/patients" element={<SignUp />} />
+        <Route path="/requests" element={<Requests />} />
+        <Route path="/home" element={<Home />} />
       
       </Routes>
     </div>
