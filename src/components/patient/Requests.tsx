9eb118ce4d7be814b04c4dad64da7e0e8e4ef320
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-04-24 13:23:02 +0100

    accept a

--- src/components/patient/Requests.tsx
+++ src/components/patient/Requests.tsx
@@ -1,58 +1,106 @@
-import axios from 'axios';
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import axios from "axios";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 14,
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-  }));
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 function Requests() {
-    const [data,setdata]=React.useState([])
-    const getRequests= async()=>{
-        try {
-          const { status, data } = await axios.get(
-            "http://localhost:3000/api/doctors/requests"
-          );
-          setdata(data)
-          console.log(data);
-        } catch (error) {
-        console.log(error);
-        
-          } 
-        }
-    React.useEffect(()=>{
-        getRequests()
-    },[])
+  const [request, setRequest] = React.useState([]);
+
+  const [data, setdata] = React.useState([]);
+  const getRequests = async () => {
+    try {
+      const {data } = await axios.get(
+        "http://localhost:3000/api/doctors/requests"
+      );
+      setdata(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const AcepteRequest = async (id) => {
+    const token=localStorage.getItem('token')
+    
+    try {
+      const {data} = await axios.get(
+        `http://localhost:3000/api/requests/accepteRequest/${id}/${token}`
+      );
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  React.useEffect(() => {
+    getRequests();
+  }, []);
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead style={{ backgroundColor: '#1DBED3', color: '#FFFFFF' }}>
-          <TableRow style={{ backgroundColor: '#1DBED3', color: '#FFFFFF' }}>
-            <StyledTableCell  align="right"style={{ backgroundColor: '#1DBED3', color: '#FFFFFF',fontSize:"21px" }}>Name</StyledTableCell>
-            <StyledTableCell align="right"style={{ backgroundColor: '#1DBED3', color: '#FFFFFF',fontSize:"21px" }}>phone number</StyledTableCell>
-            <StyledTableCell style={{ backgroundColor: '#1DBED3', color: '#FFFFFF',fontSize:"21px" }}>Description</StyledTableCell>
-            <StyledTableCell style={{ backgroundColor: '#1DBED3', color: '#FFFFFF', fontSize:"21px"}} align="">status</StyledTableCell>
+        <TableHead style={{ backgroundColor: "#1DBED3", color: "#FFFFFF" }}>
+          <TableRow style={{ backgroundColor: "#1DBED3", color: "#FFFFFF" }}>
+            <StyledTableCell
+              align="right"
+              style={{
+                backgroundColor: "#1DBED3",
+                color: "#FFFFFF",
+                fontSize: "21px",
+              }}
+            >
+              Name
+            </StyledTableCell>
+            <StyledTableCell
+              align="right"
+              style={{
+                backgroundColor: "#1DBED3",
+                color: "#FFFFFF",
+                fontSize: "21px",
+              }}
+            >
+              phone number
+            </StyledTableCell>
+            <StyledTableCell
+              style={{
+                backgroundColor: "#1DBED3",
+                color: "#FFFFFF",
+                fontSize: "21px",
+              }}
+            >
+              Description
+            </StyledTableCell>
+            <StyledTableCell
+              style={{
+                backgroundColor: "#1DBED3",
+                color: "#FFFFFF",
+                fontSize: "21px",
+              }}
+              align=""
+            >
+              status
+            </StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -61,20 +109,24 @@ function Requests() {
               <StyledTableCell component="th" scope="req">
                 {req.name}
               </StyledTableCell>
-              <StyledTableCell 
-              >{req.Patient.FullName}</StyledTableCell>
-              <StyledTableCell 
-              >{req.Patient.phone_number}</StyledTableCell>
-              <StyledTableCell 
-              >{req.message}</StyledTableCell>
-              <StyledTableCell 
-              >{req.status==="Pending"?<button>accept</button>:"Already Acepted"}</StyledTableCell>
+              <StyledTableCell>{req.Patient.FullName}</StyledTableCell>
+              <StyledTableCell>{req.Patient.phone_number}</StyledTableCell>
+              <StyledTableCell>{req.message}</StyledTableCell>
+              <StyledTableCell>
+                {req.status === "Pending" ? (
+                  <button onClick={()=>{
+                    AcepteRequest(req.id)
+                  }}>accept</button>
+                ) : (
+                  <>Already Acepted</>
+                )}
+              </StyledTableCell>
             </StyledTableRow>
           ))}
-        </TableBody>    
+        </TableBody>
       </Table>
     </TableContainer>
   );
 }
 
-export default Requests
+export default Requests;
