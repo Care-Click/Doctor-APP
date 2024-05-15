9b403ad68f4a7dd19500cef00cb9a92feb119f86
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-05-15 12:12:44 +0100

    restyling

--- src/components/patient/Requests.tsx
+++ src/components/patient/Requests.tsx
@@ -25,27 +25,31 @@ interface Request {
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     color: theme.palette.common.white,
-    textAlign: "right", // Centers the text in the header cells
-    padding: theme.spacing(1, 2), // Adjusts the padding; feel free to change the values
+    fontSize:18 ,
+    textAlign: "left", 
+    padding: theme.spacing(1, 2), 
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-    textAlign: "right", // Centers the text in the body cells
-    padding: theme.spacing(1, 2), // Adjusts the padding
+    fontSize: 15,
+    textAlign: "left", 
+    padding: theme.spacing(1, 2), 
   },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  // "&:nth-of-type(odd)": {
-  //   backgroundColor: theme.palette.action.hover,
-  // },
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
 }));
 
 function Requests() {
   const navigate = useNavigate();
+  const [acceptPopUp,setacceptPopUp] =useState(false)
   const [doctorId, setDoctorId] = useState(0);
   const [reqs, setData] = useState([]);
   const [test, setTest] = useState(false);
+  const [req, setreq] = useState();
+
   const getRequests = async () => {
     const token = localStorage.getItem("token");
     try {
@@ -69,7 +73,7 @@ function Requests() {
       );
       console.log(data);
       setTest(!test);
-      
+      setacceptPopUp(false)
     } catch (error) {
       console.log(error);
     }
@@ -79,22 +83,23 @@ function Requests() {
   }, [test]);
 
   return (
+    <div className="bg-gray-300  bg-opacity-45 p-1">
     <TableContainer
       component={Paper}
-      sx={{ mx: "auto", my: 5, boxShadow: 2, maxWidth: 1200 }}
-    >
+      sx={{ mx: "auto", my: 5, boxShadow: 2, maxWidth: 1300 }}
+     >
       <Table aria-label="customized table">
         <TableHead style={{ backgroundColor: "#00a09d", color: "#FFFFFF" }}>
           <TableRow>
-            <StyledTableCell style={{ fontSize: "17px" }}>profile picture</StyledTableCell>
-            <StyledTableCell style={{ fontSize: "17px" }}>Name</StyledTableCell>
-            <StyledTableCell style={{ fontSize: "17px" }}>
+            <StyledTableCell >profile picture</StyledTableCell>
+            <StyledTableCell >Name</StyledTableCell>
+            <StyledTableCell >
               Phone Number
             </StyledTableCell>
-            <StyledTableCell style={{ fontSize: "17px" }} align="center">
+            <StyledTableCell  align="center">
               Description
             </StyledTableCell>
-            <StyledTableCell style={{ fontSize: "17px" }}>
+            <StyledTableCell>
               Status
             </StyledTableCell>
           </TableRow>
@@ -102,12 +107,12 @@ function Requests() {
         <TableBody>
           {reqs.map((req:Request) => (
             <StyledTableRow key={req.id} className="  hover:bg-gray-300 ">
-             <td>
+             <StyledTableCell>
               <img
                 src={req?.Patient?.profile_picture}
                 alt="Profile"
                 style={{ width: 50, height: 50, borderRadius: "50%" }}
-              /></td>
+              /></StyledTableCell>
               <StyledTableCell className="text-lg">
                 {req?.Patient?.FullName}
               </StyledTableCell>
@@ -120,7 +125,8 @@ function Requests() {
               <StyledTableCell className="text-lg">
                 {req.status === "Pending" ? (
                   <button
-                    onClick={() => acceptRequest(req.id)}
+                    onClick={() => { setreq(req)
+                      setacceptPopUp(!acceptPopUp)} }
                     className="text-[#1DBED3] text-lg hover:bg-[#F26268] hover:text-white px-2 tablet:px-3 py-1 rounded transition-colors duration-300"
                   >
                     Accept
@@ -144,6 +150,48 @@ function Requests() {
         </TableBody>
       </Table>
     </TableContainer>
+    
+    {acceptPopUp && (
+  <>
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="relative w-auto my-6 mx-auto max-w-sm">
+        {/* Content */}
+        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          {/* Header */}
+          <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+            <h3 className="text-3xl font-semibold">
+              Confirmation Required
+            </h3>
+          </div>
+          {/* Body */}
+          <div className="relative p-6 flex-auto">
+            <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+              Are you sure you want to accept this request?
+            </p>
+          </div>
+          {/* Footer */}
+          <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+            <button
+              className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={() => setacceptPopUp(!acceptPopUp)}
+            >
+              Close
+            </button>
+            <button
+              
+              onClick={()=> acceptRequest(req.id)}
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+            >
+              Accept Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+   </>)}
+    </div>
   );
 }
 
