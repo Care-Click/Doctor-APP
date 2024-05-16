1357ebbd055b40457ad83ec10a9098fa8ec96d95
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-05-16 21:05:39 +0100

    styling messages

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -3,16 +3,17 @@ import "./message.css";
 import React, { useEffect, useRef, useState } from "react";
 import Message from "./Message";
 import io from "socket.io-client";
-interface message {
+
+interface Message {
   id: number;
   sender: string;
 }
 
-function Messages({currentChat}) {
-  const scrollRef = useRef();
-  const [profileDoc, setprofileDoc] = useState("");
+function Messages({ currentChat }) {
+  const messagesEndRef = useRef(null);
+  const [profileDoc, setProfileDoc] = useState("");
   const [socket, setSocket] = useState(null);
-  const [profilePat, setprofilePat] = useState("");
+  const [profilePat, setProfilePat] = useState("");
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
 
@@ -34,12 +35,15 @@ function Messages({currentChat}) {
     };
   }, [currentChat]);
 
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
 
-
-
-
-
-
+  const scrollToBottom = () => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -53,91 +57,68 @@ function Messages({currentChat}) {
         createdAt: new Date(Date.now()),
         content: newMessage,
       };
-     
+
       setMessages([...messages, messageSocket]);
       socket.emit("sendMessage", messageSocket);
-      // await axios.post("http://localhost:3000/api/messages", messageSocket);
       setNewMessage("");
     } catch (error) {
       console.log(error);
     }
   };
-  const getmessages = async () => {
-    try {
-      if (currentChat.id!==0) {
-    
-      const { data } = await axios.get(
-        `http://localhost:3000/api/conversations/${currentChat?.id}/messages`
-      );
 
-      setprofileDoc(data[0].conversation.doctor.profile_picture)
-      setprofilePat(data[0].conversation.patient.profile_picture)
-      setMessages(data);
-    }
-    } catch (error) { 
+  const getMessages = async () => {
+    try {
+      if (currentChat.id !== 0) {
+        const { data } = await axios.get(
+          `http://localhost:3000/api/conversations/${currentChat?.id}/messages`
+        );
+
+        setProfileDoc(data[0].conversation.doctor.profile_picture);
+        setProfilePat(data[0].conversation.patient.profile_picture);
+        setMessages(data);
+      }
+    } catch (error) {
       console.log(error);
     }
   };
-  
-  useEffect(() => {
-    getmessages()
-    if (scrollRef.current) {
-      scrollRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  }, [currentChat]);
-  //  useEffect(() => {
-  //   // socket.emit("joinConversation", currentChat.id,(messages) => {
-  //   //   console.log(messages);
-      
-  //   // } );
-  //      socket.on("newMessage", (data) => {
-  //       console.log(data);
-        
-  //       // const prev=[...messages]  
-  //       // prev.push(data)
-  //       // setMessages(prev);
-  //     });
-  // //   socket.on("newMessage", (data) => {
-  // //     console.log(data);
 
-  // //     setMessages(...messages,data)
-  // //   });
-  // }, []);
-  
+  useEffect(() => {
+    getMessages();
+  }, [currentChat]);
+
   return (
     <div className="chatBox">
-    <div className="chatBoxWrapper">
-      {messages.length !== 0 ? (
-        <div className="chatBoxTop" >
-          {messages.map((message: message) => {
-            return (
-              <div  key={message.id}>
-                <Message 
-                profileDoc={profileDoc}
-                profilePat={profilePat}
+      <div className="chatBoxWrapper">
+        {messages.length !== 0 ? (
+          <div className="chatBoxTop">
+            {messages.map((message: Message) => (
+              <div key={message.id}>
+                <Message
+                  profileDoc={profileDoc}
+                  profilePat={profilePat}
                   message={message}
                   own={message.sender === "Doctor"}
                 />
               </div>
-            );
-          })}
-           <div className="chatBoxBottom">
-            <textarea
-              className="chatIntput"
-              placeholder="write somthing"
-              onChange={(e) => setNewMessage(e.target.value)}
-              value={newMessage}
-            ></textarea>
-            <button className="buttonSend" onClick={handleSubmit}>
-              send
-            </button>
+            ))}
+            <div ref={messagesEndRef} />
+            <div className="chatBoxBottom">
+            <input
+               className="w-full h-12 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
+               type="text"
+               placeholder="Write your message..."
+               onChange={(e) => setNewMessage(e.target.value)}
+               value={newMessage}/>
+              <button className="buttonSend" onClick={handleSubmit}>
+                Send
+              </button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <span className="nochat">Open a conversation to start a chat</span>
-      )}
+        ) : (
+          <span className="nochat">Open a conversation to start a chat</span>
+        )}
+      </div>
     </div>
-  </div>
   );
 }
 
