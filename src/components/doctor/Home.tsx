a626e5fb345141bb8e5b9e4c950e4bb290f471e0
OumaymaMansour <mansour.oumeima@gmail.com>
2024-05-20 16:11:46 +0100

    restyling home page

--- src/components/doctor/Home.tsx
+++ src/components/doctor/Home.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import Calendar from './Calander.tsx'
-import Patients from '../patient/Patients.tsx'
+
 
 function Home() {
   return (
-    <div style={{ backgroundImage: "url('/src/assets/images/bg.jpg')" 
+    <div style={{ backgroundImage: "url('/src/assets/images/rapport.png')" 
     }} 
-    className=" flex justify-center bg-cover bg-no-repeat " >
-    
-    <div style={{ flex: '2' , borderRight: '4px solid gray'}}>
+    className=" flex justify-center h-screen bg-cover overflow-hidden p-40" >
         <Calendar />
-      </div>
-      <div style={{ flex: '1' , backgroundColor: '#ECECEC'}}>
-        <Patients />
-      </div>
     </div>
     )
 }
